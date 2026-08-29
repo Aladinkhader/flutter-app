7f42bcb279551517eed78d8c3b2c456f00a9d200
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-08-29 09:44:39 +0200

    Create lecture.dart

---
+++ lib/models/lecture.dart
@@ -0,0 +1,13 @@
+class Lecture {
+  final String title;
+  final String section;
+  final String audioUrl;
+  final String identifier;
+
+  Lecture({
+    required this.title,
+    required this.section,
+    required this.audioUrl,
+    required this.identifier,
+  });
+}
