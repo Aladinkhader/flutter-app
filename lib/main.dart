1c80a28123a1db1398611339dd4c94f7d5701449
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-08-29 14:33:31 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'theme/app_theme.dart';
 import 'screens/splash_screen.dart';
+import 'services/favorites_service.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await FavoritesService.instance.init();
 
   ErrorWidget.builder = (FlutterErrorDetails details) {
     return Material(
